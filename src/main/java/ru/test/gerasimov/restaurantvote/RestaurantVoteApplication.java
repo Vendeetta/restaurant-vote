de71c14c097001d6f51493254641aadf35f021ac
Vendeetta <7865330@gmail.com>
2023-02-27 16:59:47 +0300

    init commit

---
+++ src/main/java/ru/test/gerasimov/restaurantvote/RestaurantVoteApplication.java
@@ -0,0 +1,13 @@
+package ru.test.gerasimov.restaurantvote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantVoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantVoteApplication.class, args);
+    }
+
+}
